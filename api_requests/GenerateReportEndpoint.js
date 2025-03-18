3cf62141935866e3b9572e0952219cbac51a257f
shankar20487 <Shankar.Bhanja@fullsteam.com>
2025-03-18 21:40:59 +0530

    add api related files

---
+++ api_requests/GenerateReportEndpoint.js
@@ -0,0 +1,81 @@
+// Inlcude playwright module
+import {expect } from '@playwright/test';
+import endpoint from '../config/endpoint';
+
+
+// create cl
+export default class GenerateReportEndpoint {
+        constructor(baseURL,reportName) {
+            this.baseURL = baseURL;
+            this.reportName= reportName;
+        }
+    
+        async generateReportApi(headers = {},data) {
+            try {
+                const apiURL = this.baseURL + `${endpoint.endpoints.generatereport}`
+                console.log(apiURL)
+                const response = await fetch(`${apiURL}`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        ...headers
+                    },
+                    body: JSON.stringify(data) // Convert the data object to a JSON string
+                   
+                });
+            console.log(response)
+            return response;
+            } catch (error) {
+                console.error("Error in POST request:", error);
+                throw error; // Re-throw for handling
+            }
+        }
+
+           
+        async generateEzClaimPayReceiptPayload(startDate,endDate,patients,sortby="",paymentMethod='',transactionID='',lastDigt=''){
+            let payload;
+            if(patients!=undefined){
+                 payload ={
+                    reportId: this.reportName,
+                    options: {
+                      message: '',
+                      sortby: sortby ||'patientName',
+                      patient: [patients],
+                      paymentMethod: [paymentMethod],
+                      paymentRef: transactionID || '',
+                      paymentAdditionalRef:lastDigt || '',
+                      usePatientBillingAddress: true,
+                      paymentDate: { startDate: startDate, endDate: endDate },
+                      paymentEnteredDate: { startDate: startDate, endDate: endDate}
+                    }
+                  }
+               
+
+            }
+            
+            else{
+                payload ={
+                    reportId: this.reportName,
+                    options: {
+                      message: '',
+                      sortby: sortby,
+                      patient: [],
+                      paymentMethod: [paymentMethod],
+                      paymentRef: '',
+                      paymentAdditionalRef: '',
+                      usePatientBillingAddress: false,
+                      paymentDate: { startDate: startDate, endDate: endDate },
+                      paymentEnteredDate: { startDate: startDate, endDate: endDate}
+                    }
+                  }
+            }
+             return payload;
+            }
+
+        }
+    
+    
+   
+    
+
+
